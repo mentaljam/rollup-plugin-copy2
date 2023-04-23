f408db3e5aa1e9c6fad07b9bc2ac5142a5e6ddb8
Petr Tsymbarovich <petr@tsymbarovich.ru>
2023-04-23 11:26:09 +0300

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import * as fs from 'fs'
+import {promises as fs} from 'fs'
 import * as path from 'path'
-import {Plugin} from 'rollup'
+import type {Plugin, PluginContext} from 'rollup'
 import {glob} from 'glob'
 
 
@@ -14,58 +14,82 @@ interface IPluginCopy2Options {
 
 type RollupPluginCopy2 = (options: IPluginCopy2Options) => Plugin
 
+const getAssetPathsMapper = (
+  ctx: PluginContext,
+  srcDir: string,
+) => {
+  const normalizeSrcFile = (srcFile: string) => {
+    srcFile = path.normalize(srcFile)
+
+    if (!path.isAbsolute(srcFile)) {
+      srcFile = path.resolve(srcDir, srcFile)
+    }
+
+    if (process.platform === 'win32') {
+      srcFile = srcFile.replace(/\\+/g, '/')
+    }
+
+    return srcFile
+  }
+
+  return (asset: CopyEntry): [string, string, boolean] => {
+    if (typeof asset === 'string') {
+      return [normalizeSrcFile(asset), asset, false]
+    }
+
+    if (Array.isArray(asset) && asset.length === 2) {
+      const [srcFile, fileName] = asset
+      return [normalizeSrcFile(srcFile), fileName, true]
+    }
+
+    ctx.error('Asset should be a string or a pair of strings [string, string]')
+  }
+}
+
 const rollupPluginCopy2: RollupPluginCopy2 = (options) => ({
   name: 'copy2',
-  generateBundle() {
+
+  buildStart() {
     if (!(options && options.assets && Array.isArray(options.assets))) {
       this.error('Plugin options are invalid')
     }
+
+    if (!options.outputDirectory && options.notEmitFiles) {
+      this.error('`notEmitFiles` is set but `outputDirectory` is not provided')
+    }
+  },
+
+  async generateBundle() {
     const {assets, notEmitFiles} = options
     if (!assets.length) {
       this.warn('An empty list of assets was passed to plugin options')
       return
     }
+
     const srcDir = process.cwd()
-    let outDir   = options.outputDirectory
-    if (outDir) {
-      if (!path.isAbsolute(outDir)) {
-        outDir = path.resolve(srcDir, outDir)
-      }
-    } else if (notEmitFiles) {
-      this.error('`notEmitFiles` is set but `outputDirectory` is not provided')
+    let {outputDirectory: outDir} = options
+    if (outDir && !path.isAbsolute(outDir)) {
+      outDir = path.resolve(srcDir, outDir)
     }
-    for (const asset of assets) {
-      let srcFile:  string
-      let fileName: string
-      const assetIsPair = Array.isArray(asset) && asset.length === 2
-      if (typeof asset === 'string') {
-        srcFile  = asset
-        fileName = asset
-      } else if (assetIsPair) {
-        srcFile  = asset[0]
-        fileName = asset[1]
-      } else {
-        this.error('Asset should be a string or a pair of strings [string, string]')
-      }
-      srcFile = path.normalize(srcFile)
-      if (!path.isAbsolute(srcFile)) {
-        srcFile = path.resolve(srcDir, srcFile)
-      }
-      if (process.platform === 'win32') {
-        srcFile = srcFile.replace(/\\+/g, '/')
-      }
-      const files = glob.sync(srcFile)
+
+    const pathMapper = getAssetPathsMapper(this, srcDir)
+
+    for (let [srcFile, fileName, assetIsPair] of assets.map(pathMapper)) {
+      const files = await glob(srcFile)
       if (files.length === 0) {
         this.error(`"${srcFile}" doesn't exist`)
       }
+
       if (files.length > 1 && assetIsPair) {
         this.error(`Cannot mix glob "${srcFile}" pattern for assets with [string, string] notation`)
       }
+
       for (const file of files) {
         if (!assetIsPair) {
           fileName = path.relative(srcDir, file)
         }
-        const source = fs.readFileSync(file)
+
+        const source = await fs.readFile(file)
         if (!notEmitFiles) {
           this.emitFile({
             fileName,
@@ -73,10 +97,11 @@ const rollupPluginCopy2: RollupPluginCopy2 = (options) => ({
             type: 'asset',
           })
         }
+
         if (outDir) {
           const filePath = path.resolve(outDir, fileName)
-          fs.mkdirSync(path.dirname(filePath), {recursive: true})
-          fs.writeFileSync(filePath, source)
+          await fs.mkdir(path.dirname(filePath), {recursive: true})
+          await fs.writeFile(filePath, source)
         }
       }
     }
